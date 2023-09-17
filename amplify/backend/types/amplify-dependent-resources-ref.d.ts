ee35a92f92d8f502ea517c61a8f27831e7ce42d3
Atiqah(아티카) <n.atiqah.juvono@gmail.com>
2023-09-17 22:46:26 +0900

    last vid

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "contacthost": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "contacthost5eeabf5a": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "contactHost": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { Route, Routes } from 'react-router-dom';
+
+import { Amplify } from 'aws-amplify';
+import awsExports from './aws-exports';
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import '@aws-amplify/ui-react/styles.css';
+
+import SiteNav from './components/common/SiteNav';
+import SiteFooter from './components/common/SiteFooter';
+import HomePage from './components/home/HomePage';
+import LoginPage from './components/auth/LoginPage';
+import RegisterPage from './components/auth/RegisterPage';
+import ValidatePage from './components/auth/ValidatePage';
+import Contacts from './components/contacts/Contacts';
+
+Amplify.configure(awsExports);
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(false)
+
+  function updateAuthStatus(authStatus) {
+    setIsAuthenticated(authStatus)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <SiteNav isAuthenticated={isAuthenticated} updateAuthStatus={updateAuthStatus} />
+      <Routes>
+        <Route path='*' element={<HomePage isAuthenticated={isAuthenticated} />} />
+        <Route path='/' exact={true} element={<HomePage isAuthenticated={isAuthenticated} />} />
+        <Route path='/login' element={<LoginPage updateAuthStatus={updateAuthStatus} />} />
+        <Route path='/register' element={<RegisterPage />} />
+        <Route path='/validate' element={<ValidatePage />} />
+        <Route path='/contacts' element={<Contacts isAuthenticated={isAuthenticated} />} />
+      </Routes>
+      <SiteFooter />
     </div>
-  );
-}
+  )
+};
 
 export default App;
